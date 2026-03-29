a13115e17caf278c7bd9760816b260dc8f6400eb
Anthony <Anthony.ruelland16@gmail.com>
2026-03-29 13:23:17 -0400

    Menu Principal : Ajout des niveaux 2 et 3 débloqués de base

--- levels.js
+++ levels.js
@@ -76,4 +76,55 @@ addP(15, [0,0,0,0,0,0,0,0,0,1]);
 addP(30, [0,0,0,0,0,0,0,0,0,1]); // Une gigantesque piste droite finale sereine pour avoir ses 100% sans aucun mur à percuter !
 
 levelsData[1].mapColumns = cols;
+
+
+// ==========================================
+// NIVEAU 2 : BACK ON TRACK
+// ==========================================
+levelsData[2] = {
+    name: "Back on Track",
+    bgColor: "#2a0a2a", // Violet foncé
+    primaryColor: "#ff00ff", // Magenta
+    obstacleColor: "#441144",
+    speed: 3.5, 
+    mapColumns: []
+};
+
+let cols2 = [];
+function addP2(count, colArray) { for(let i=0; i<count; i++) cols2.push([...colArray]); }
+
+addP2(20, [0,0,0,0,0,0,0,0,0,1]); // Début
+cols2.push([0,0,0,0,0,0,0,0,2,1]); // Pique
+addP2(15, [0,0,0,0,0,0,0,0,0,1]);
+cols2.push([0,0,0,0,0,0,0,0,1,1]); // Escalier
+cols2.push([0,0,0,0,0,0,0,1,1,1]);
+addP2(5,  [0,0,0,0,0,0,0,1,1,1]);
+cols2.push([0,0,0,0,0,0,0,0,2,1]); // Pique vicieux en redescente
+addP2(30, [0,0,0,0,0,0,0,0,0,1]); // Fin
+levelsData[2].mapColumns = cols2;
+
+
+// ==========================================
+// NIVEAU 3 : POLARGEIST
+// ==========================================
+levelsData[3] = {
+    name: "Polargeist",
+    bgColor: "#0a2a2a", // Cyan sombre
+    primaryColor: "#00ff88", // Vert printemps
+    obstacleColor: "#114444",
+    speed: 4.0, 
+    mapColumns: []
+};
+
+let cols3 = [];
+function addP3(count, colArray) { for(let i=0; i<count; i++) cols3.push([...colArray]); }
+
+addP3(15, [0,0,0,0,0,0,0,0,0,1]);
+cols3.push([0,0,0,0,0,3,0,0,0,1]); // Fusée directe !
+addP3(10, [0,0,0,0,0,0,0,0,0,1]);
+addP3(10, [1,1,0,0,0,0,0,0,1,1]); // Tunnel de vol
+addP3(30, [0,0,0,0,0,0,0,0,0,1]);
+levelsData[3].mapColumns = cols3;
+
+
 window.levelsData = levelsData;
